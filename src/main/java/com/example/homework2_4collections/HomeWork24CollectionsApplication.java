849c7619ec6a830d815feb81039268cdb8ccbf33
Olga <dochnohi@yandex.ru>
2022-09-29 17:33:36 +0300

    Initial commit

---
+++ src/main/java/com/example/homework2_4collections/HomeWork24CollectionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.homework2_4collections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWork24CollectionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeWork24CollectionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/homework2_4collections/HomeWork24CollectionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homework2_4collections;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWork24CollectionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
